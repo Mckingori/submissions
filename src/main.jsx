a3bb174c5ca6042c876e188359d0d3f7db366bc9
Macharia <machariakingori001@gmail.com>
2026-03-18 02:05:18 +0300

    repeating calls to render

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,16 @@
-
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+let counter = 1
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const refresh = () => {//the render method
+    root.render(
+        <App counter={counter} />
+    )
+}
+setInterval(() => {//making repeatted calls  to the render maethod
+    refresh()
+    counter += 1
+}, 1000)
+
