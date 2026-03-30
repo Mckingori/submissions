5b69779aba4ddba2a81c2787dd9124afb8e27281
Macharia <machariakingori001@gmail.com>
2026-03-30 16:11:01 +0300

    learnt event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,64 @@
-const App = (props) =>{
-  const {counter} = props
+import { useState } from "react"
+
+const History = (props) => {
+  if(props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing buttons
+      </div>
+    )
+  }
   return (
-    <div>{counter}</div>
+    <div>
+      button press History: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+  
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const  [allClicks, setAll] = useState([])
+
+
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+  const handleRightClick = () =>{
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+
+  
+
+  return (
+    <div>
+      {left}
+    <Button onClick={handleLeftClick} text='left'/>
+    <Button onClick={handleRightClick} text='right'/>
+      {right}
+     <History allClicks={allClicks} />
+    </div>
+)
+}
 
 
 export default App
+
+
+
+/*const newClicks = {
+  left: clicks.left,
+  right: clicks.right + 1
+}
+setClicks(newClicks)
+}*/
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,5 @@
 import ReactDOM from 'react-dom/client'
+
 import App from './App'
 
-let counter = 1
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-const refresh = () => {//the render method
-    root.render(
-        <App counter={counter} />
-    )
-}
-setInterval(() => {//making repeatted calls  to the render maethod
-    refresh()
-    counter += 1
-}, 1000)
-
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
